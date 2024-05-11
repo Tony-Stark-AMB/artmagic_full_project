92663975f15e3d25e7bd1ae18ff6f948655fe680
Tony-Stark-AMB <tony.stark.amb@gmail.com>
2024-05-11 18:13:06 +0300

    add some responsivness to index.html

--- static/js/index-carousel.js
+++ static/js/index-carousel.js
@@ -1,5 +1,46 @@
 import {Swiper, Navigation, Pagination} from "./import.js";
 
+function debounce(func, delay) {
+  let timerId;
+  
+  return function(...args) {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+
+// Your resize event handler function
+function handleResize() {
+  // Your code here
+  console.log("here");
+  productsCarousel.breakpoints = breakpointsProductsCarousel();
+  new Swiper(".products-index__list__wrap", productsCarousel);
+}
+
+// Debounce the handleResize function with a delay of 300ms
+const debouncedResizeHandler = debounce(handleResize, 300)
+
+const breakpointsProductsCarousel = () => {
+  return {
+    1800: {
+      slidesPerView: 5,
+      slidesPerGroup: 5,
+    },
+    1400: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    },
+    1200: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  }
+}
+
+window.addEventListener("resize", debouncedResizeHandler);
+
 export const {mainBanner, productsCarousel} = {
  
   mainBanner: {
@@ -17,6 +58,7 @@ export const {mainBanner, productsCarousel} = {
   productsCarousel: {
     slidesPerView: 5,
     slidesPerGroup: 5,
+    spaceBetween: 10,
     speed: 2000,
     pagination: {
       clickable: true,
@@ -24,6 +66,11 @@ export const {mainBanner, productsCarousel} = {
       renderBullet: function (index, className) {
         return '<span class="' + className + '">' + (index + 1) + "</span>";
       },
+    },
+    breakpoints: {
+      
+      
+
     },
   },
   modules: [Navigation, Pagination]
