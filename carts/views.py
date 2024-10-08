c83fc392c1c9020d30bf3f29fc6f7d1a9cd4d2b0
Dmitriy-Bozdrov <bozdrov@gmail.com>
2024-10-08 20:40:14 +0300

    structured the class

--- carts/views.py
+++ carts/views.py
@@ -3,226 +3,123 @@ from django.contrib import messages
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
-from django.utils.crypto import get_random_string
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-from carts.models import Cart, Order
+from carts.models import Order
 from django.views import View
-from carts.utils import get_user_carts, generate_excel
-from django.core.mail import send_mail
 from django.core.mail import EmailMessage
-from products.models import Products
-from delivery.models import DeliveryOption
-from liqpay_app.models import Payment
 from .decryption_ref import get_city_name, get_area_name, get_department_name
-from openpyxl import Workbook
-import os
-from datetime import datetime
 import json
 import logging
-from django.conf import settings
-
-
-def cart_add(request):
-    product_id = request.POST.get("product_id")
-
-    product = Products.objects.get(id=product_id)
-
-    if request.user.is_authenticated:
-        carts = Cart.objects.filter(user=request.user, product=product)
-
-        if carts.exists():
-            cart = carts.first()
-            if cart:
-                cart.quantity += 1
-                cart.save()
-        else:
-            Cart.objects.create(user=request.user, product=product, quantity=1)
-
-    else:
-        carts = Cart.objects.filter(
-            session_key=request.session.session_key, product=product)
-
-        if carts.exists():
-            cart = carts.first()
-            if cart:
-                cart.quantity += 1
-                cart.save()
-        else:
-            Cart.objects.create(
-                session_key=request.session.session_key, product=product, quantity=1)
-
-    user_cart = get_user_carts(request)
-    cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
-
-    response_data = {
-        "message": "Товар добавлен в корзину",
-        "cart_items_html": cart_items_html,
-    }
-
-    return JsonResponse(response_data)
-
-
-def cart_change(request):
-    cart_id = request.POST.get("cart_id")
-    quantity = request.POST.get("quantity")
-
-    cart = Cart.objects.get(id=cart_id)
-
-    cart.quantity = quantity
-    cart.save()
-    updated_quantity = cart.quantity
-
-    cart = get_user_carts(request)
-    cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": cart}, request=request)
-
-    response_data = {
-        "message": "Количество изменено",
-        "cart_items_html": cart_items_html,
-        "quaantity": updated_quantity,
-    }
-
-    return JsonResponse(response_data)
-
-
-def cart_remove(request):
-    cart_id = request.POST.get("cart_id")
-
-    cart = Cart.objects.get(id=cart_id)
-    quantity = cart.quantity
-    cart.delete()
-
-    user_cart = get_user_carts(request)
-    cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
-
-    response_data = {
-        "message": "Товар удален",
-        "cart_items_html": cart_items_html,
-        "quantity_deleted": quantity,
-    }
-
-    return JsonResponse(response_data)
 
 
 logger = logging.getLogger(__name__)
 
 class ProcessOrderView(View):
 
+    payment_options = {
+        'liqpay': "Онлайн-оплата банківською карткою",
+        'payment_card': "Оплата за реквізитами",
+        'payment_real': "Оплата у точці видачі"
+    }
+
+    delivery_options = {
+        'artmagic_department': "Самовивіз",
+        'new_post_department': "Відділення Нової пошти",
+        'new_post_packing': "Поштомат Нової пошти",
+        'new_post_address': "Кур'єрська доставка Нової пошти",
+        'ukr_post': "Укрпошта"
+    }
+
+    email_owner = 'Asgeron90@gmail.com'
+
     def post(self, request):
-
-        payment_options = {
-                'liqpay': "Онлайн-оплата банківською карткою",
-                'payment_card': "Оплата за реквізитами",
-                'payment_real': "Оплата у точці видачі"
-            }        
-        delivery_options = {
-                'artmagic_department': "Самовивіз",
-                'new_post_department': "Відділення Нової пошти",
-                'new_post_packing': "Поштомат Нової пошти",
-                'new_post_address': "Кур'єрська доставка Нової пошти",
-                'ukr_post': "Укрпошта"
-            }
-        
         try:
-            body_unicode = request.body.decode('utf-8')
-            data = json.loads(body_unicode)
-            data_delivery = data.get('selectedDelivery')
-            data_payment = data.get('selectedPayment')
-            logger.debug('Received data: %s', data)
+            data = self.parse_request_data(request)
+            delivery_method, address = self.get_delivery_info(data)
+            self.validate_required_fields(data)
+            order_number = self.generate_order_number()
+            user = request.user if request.user.is_authenticated else None
+            
+            order = self.create_order(data, order_number, user, address)
+            context = self.prepare_email_context(data, delivery_method, order_number)
 
-            name = data.get('name')
-            phone = data.get('phone')
-            email = data.get('email')
-            total_price = data.get('amount')
-            delivery_method = delivery_options.get(data_delivery, "")   
-            payment = payment_options.get(data_payment, "")
-            address = data.get('address', '')
+            self.send_email(self.email_owner, context, f"Замовлення №: {order_number}", 'carts/email_template.html')
+            self.send_email(data.get('email'), context, 'Ваше замовлення прийняте', 'users/email_template_user.html')
+
+            return JsonResponse({'status': 'success', 'message': 'Заказ успешно отправлен'}, status=200)
+
+        except (json.JSONDecodeError, ValueError) as e:
+            logger.error('Error processing order: %s', e)
+            return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
+        except Exception as e:
+            logger.error('Unexpected error: %s', e)
+            return JsonResponse({'status': 'error', 'message': 'Ошибка при обработке заказа'}, status=500)
+
+    def parse_request_data(self, request):
+        body_unicode = request.body.decode('utf-8')
+        data = json.loads(body_unicode)
+        logger.debug('Received data: %s', data)
+        return data
+
+    def get_delivery_info(self, data):
+        data_delivery = data.get('selectedDelivery')
+        delivery_method = self.delivery_options.get(data_delivery, "")
+        address = data.get('address', '')
+        
+        # Установка адреса в зависимости от метода доставки
+        if delivery_method == "Самовивіз":
+            address = 'м. Дніпро, Вул. Якова Самарського 5, к. 7'
+        elif delivery_method in ["Відділення Нової пошти", "Поштомат Нової пошти"]:
             area_value = get_area_name(data['area'])
             city_value = get_city_name(data['city'])
             department_value = get_department_name(data['department'], data['city'])
-            products = data.get('products')
-            
-            if delivery_method == "Самовивіз":
-                address = 'м. Дніпро, Вул. Якова Самарського 5, к. 7'            
-            if delivery_method in ["Відділення Нової пошти", "Поштомат Нової пошти"]:
-                address = f'{area_value } область, {city_value}, {department_value}'
-            if delivery_method == "Кур'єрська доставка Нової пошти":
-                address = f'{area_value } область, {city_value}, {address}'    
+            address = f'{area_value} область, {city_value}, {department_value}'
+        elif delivery_method == "Кур'єрська доставка Нової пошти":
+            area_value = get_area_name(data['area'])
+            city_value = get_city_name(data['city'])
+            address = f'{area_value} область, {city_value}, {address}'
+        
+        return delivery_method, address
 
-            # Проверка на наличие обязательных полей
-            if not (name, phone, email, products):
-                logger.error('Missing required fields in data')
-                return JsonResponse({'status': 'error', 'message': 'Missing required fields'}, status=400)
+    def validate_required_fields(self, data):
+        required_fields = ['name', 'phone', 'email', 'products']
+        if not all(data.get(field) for field in required_fields):
+            raise ValueError('Missing required fields in data')
 
-            # Генерация уникального номера заказа
-            while True:
-                order_number = str(random.randint(10000000, 99999999))
-                if not Order.objects.filter(order_number=order_number).exists():
-                    break
-            user = request.user if request.user.is_authenticated else None
+    def generate_order_number(self):
+        while True:
+            order_number = str(random.randint(10000000, 99999999))
+            if not Order.objects.filter(order_number=order_number).exists():
+                return order_number
 
-            order = Order.objects.create(
-                order_number=order_number,
-                user = user,
-                name=name,
-                phone=phone,
-                email=email,
-                payment=payment,
-                address=address,
-                total_price=total_price,
-                products=products  # Сохранение продуктов как JSON-объект
-            )
+    def create_order(self, data, order_number, user, address):
+        return Order.objects.create(
+            order_number=order_number,
+            user=user,
+            name=data.get('name'),
+            phone=data.get('phone'),
+            email=data.get('email'),
+            payment=self.payment_options.get(data.get('selectedPayment', ""), ""),
+            address=address,
+            total_price=data.get('amount'),
+            products=data.get('products')  # Сохранение продуктов как JSON-объект
+        )
 
-            # Параметры для писем
-            context = {
-                'name': name,
-                'phone': phone,
-                'email': email,
-                'payment': payment,
-                'address': address,
-                'products': products,
-                'total_price': total_price,
-                'delivery_method': delivery_method,
-                'order_number': order_number
-            }
-            
-            # Формирование письма владельцу сайта
-            subject_owner = f"Замовлення №: {order_number}"
-            html_message_owner = render_to_string('carts/email_template.html', context)
-            recipient_list_owner = ['Asgeron90@gmail.com']
+    def prepare_email_context(self, data, delivery_method, order_number):
+        return {
+            'name': data.get('name'),
+            'phone': data.get('phone'),
+            'email': data.get('email'),
+            'payment': self.payment_options.get(data.get('selectedPayment', ""), ""),
+            'address': data.get('address', ''),
+            'products': data.get('products'),
+            'total_price': data.get('amount'),
+            'delivery_method': delivery_method,
+            'order_number': order_number
+        }
 
-            try:
-                email_message_owner = EmailMessage(subject_owner, html_message_owner, 'Asgeron90@gmail.com', recipient_list_owner)
-                email_message_owner.content_subtype = "html"
-                email_message_owner.send()
-                logger.debug('Email sent successfully to %s', recipient_list_owner)
-            except Exception as e:
-                logger.error('Error sending email to owner: %s', e)
-                return JsonResponse({'status': 'error', 'message': 'Ошибка при отправке электронной почты владельцу'}, status=500)
-
-            # Формирование письма пользователю
-            subject_user = 'Ваше замовлення прийняте'
-            html_message_user = render_to_string('users/email_template_user.html', context)
-            recipient_list_user = [email]
-
-            try:
-                email_message_user = EmailMessage(subject_user, html_message_user, 'Asgeron90@gmail.com', recipient_list_user)
-                email_message_user.content_subtype = "html"
-                email_message_user.send()
-                logger.debug('Email sent successfully to %s', recipient_list_user)
-                return JsonResponse({'status': 'success', 'message': 'Заказ успешно отправлен'}, status=200)
-            except Exception as e:
-                logger.error('Error sending email to user: %s', e)
-                return JsonResponse({'status': 'error', 'message': 'Ошибка при отправке электронной почты пользователю'}, status=500)
-
-        except json.JSONDecodeError as e:
-            logger.error('JSON decode error: %s', e)
-            return JsonResponse({'status': 'error', 'message': 'Invalid JSON format'}, status=400)
-
-        except Exception as e:
-            logger.error('Error processing order: %s', e)
-            return JsonResponse({'status': 'error', 'message': 'Ошибка при обработке заказа'}, status=500)
+    def send_email(self, recipient, context, subject, template_path):
+        html_message = render_to_string(template_path, context)
+        email_message = EmailMessage(subject, html_message, self.email_owner, [recipient])
+        email_message.content_subtype = "html"
+        email_message.send()
+        logger.debug('Email sent successfully to %s', recipient)
