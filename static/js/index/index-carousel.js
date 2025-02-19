45b8fbf677e03386db7e91580981e61d7583e79b
Tony-Stark-AMB <tony.stark.amb@gmail.com>
2025-02-19 22:08:05 +0200

    add some changes to header call, and add alt to products on index-page

--- static/js/index/index-carousel.js
+++ static/js/index/index-carousel.js
@@ -29,7 +29,7 @@ class IndexProducts extends PageProducts{
     const productHTML = `
         <div class="products-${this.pageName}__item product-item" id="${id}">
             <div class="products-${this.pageName}__item__img__wrap">
-                <img class="products-${this.pageName}__item__img" src="${image}" />
+                <img class="products-${this.pageName}__item__img" src="${image}" alt="${name}" />
             </div>
             <div class="products-index__item__content">
                 <p class="products-${this.pageName}__item__title">${name}</p>
