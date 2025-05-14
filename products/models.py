9947387b3d6c00a614f66c41e3bd71754cf284be
Dmitriy-Bozdrov <bozdrov@gmail.com>
2025-05-14 20:26:44 +0300

    corrected method save in model Category and Products, added descriptions main and category page, added method replace in description field

--- products/models.py
+++ products/models.py
@@ -112,10 +112,10 @@ class Category(MPTTModel):
     def get_absolute_url(self):
         return reverse('sub_categories', kwargs={'slug': self.slug})
     
-    def save(self, args, **kwargs):
+    def save(self, *args, **kwargs):
         if not self.slug or self.slug != slugify(unidecode(self.name)):
             self.slug = slugify(unidecode(self.name))
-        super().save(args, kwargs)
+        super().save(*args, **kwargs)
 
 
 class Stocks(models.Model):
@@ -256,7 +256,7 @@ class Products(models.Model):
     def get_absolute_url(self):
         return reverse('detaile_product', kwargs={'id': self.id})
     
-    def save(self, *args, kwargs):
+    def save(self, *args, **kwargs):
         if not self.slug or self.slug != slugify(unidecode(self.name)):
             self.slug = slugify(unidecode(self.name))
         super().save(*args, **kwargs)
