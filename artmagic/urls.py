7624f08a9f53fbb91bcb1e855a224c0b4c37f2dd
Tony-Stark-AMB <tony.stark.amb@gmail.com>
2025-05-28 23:05:43 +0300

    add changes with h(s)

--- artmagic/urls.py
+++ artmagic/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path("ckeditor5/", include('django_ckeditor_5.urls')),
 ]
 
-handler404 = 'products.views.tr_handler404'
+# handler404 = 'products.views.tr_handler404'
 handler500 = 'products.views.tr_handler500'
 
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -634,14 +634,14 @@ def generate_google_merchant_feed(request):
     return response
 
 
-def tr_handler404(request, exception):
-    """
-    Обработка ошибки 404
-    """
-    return render(request=request, template_name='products/error_page.html', status=404, context={
-        'title': 'Сторінку не знайдено',
-        'error_message': 'Неправильно набрано адресу або такої сторінки на сайті більше не існує.',
-    })
+# def tr_handler404(request, exception):
+#     """
+#     Обработка ошибки 404
+#     """
+#     return render(request=request, template_name='products/error_page.html', status=404, context={
+#         'title': 'Сторінку не знайдено',
+#         'error_message': 'Неправильно набрано адресу або такої сторінки на сайті більше не існує.',
+#     })
 
 
 def tr_handler500(request):
